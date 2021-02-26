39973d586d38246d5c66547f66cb81df026b6de9
Talha Tariq <67512109+talha112197@users.noreply.github.com>
2021-02-26 20:22:54 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,242 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if(scroll < 60) {
+        $('#navbar').removeClass('fixed');
+    } else {
+        $('#navbar').addClass('fixed');
+    }
+});
+
+
+$('document').ready(function() {
+    $('.team-members').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  pauseOnHover: true,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.testimonials').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  pauseOnHover: true,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.clients').slick({
+  dots: true,
+  arrows: true,
+  infinite: true,
+  autoplay: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.blogs').slick({
+  dots: true,
+  arrows: true,
+  infinite: false,
+  autoplay: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+});
+
+});
+
+$(function () {
+
+    $(".progress-row").waypoint(function () {
+        $(".progress-bar").each(function () {
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 2000);
+        });
+
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });    
+}); 
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+
+
+$('document').ready(function() {
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  // options
+  itemSelector: '.filterCol',
+  layoutMode: 'masonry',
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+});
+
+
+$( document ).ready( function() {
+	
+	jQuery(function ($) {
+    	"use strict";
+    
+    	var counterUp = window.counterUp["default"]; // import counterUp from "counterup2"
+    
+    	var $counters = $(".counter-value");
+    
+    	/* Start counting, do this on DOM ready or with Waypoints. */
+		$counters.each(function (ignore, counter) {
+			var waypoint = new Waypoint( {
+				element: $(this),
+				handler: function() { 
+					counterUp(counter, {
+						duration: 1000,
+						delay: 10
+					}); 
+					this.destroy();
+				},
+				offset: 'bottom-in-view',
+			} );
+		});
+
+	});
+ });
+
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
